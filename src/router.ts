ae341ee60a3926f375c972193b4f0290ee2c393b
randoksi <monyerandy15@gmail.com>
2024-08-07 00:03:20 +0100

    Added pages and components

--- src/router.ts
+++ src/router.ts
@@ -1,24 +1,193 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from './views/Home.vue'
-// import About from './views/About.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
+  // Coin
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/coin/:id",
+    name: "Coin",
+    component: () => import("./views/Coin.vue"),
   },
-  
-//   {
-//     path: '/about',
-//     name: 'About',
-//     component: About
-//   }
-]
+
+  // Dao
+  {
+    path: "/dao",
+    name: "Dao",
+    component: () => import("./views/Dao.vue"),
+  },
+
+  // Defi
+  {
+    path: "/defi",
+    name: "Defi",
+    component: () => import("./views/Defi.vue"),
+  },
+
+  // Dex
+  {
+    path: "/dex",
+    name: "Dex",
+    component: () => import("./views/Dex.vue"),
+  },
+
+  // Exchange
+  {
+    path: "/exchange",
+    name: "Exchange",
+    component: () => import("./views/Exchange.vue"),
+  },
+
+  // Favorites
+  {
+    path: "/favorites",
+    name: "Favorites",
+    component: () => import("./views/Favorites.vue"),
+  },
+
+  // FanToken
+  {
+    path: "/fan-token",
+    name: "FanToken",
+    component: () => import("./views/FanToken.vue"),
+  },
+
+  // Football Club
+  {
+    path: "/football-club",
+    name: "FootballClub",
+    component: () => import("./views/FootballClub.vue"),
+  },
+
+  // Gainers
+  {
+    path: "/gainers",
+    name: "Gainers",
+    component: () => import("./views/Gainers.vue"),
+  },
+
+  // Gaming
+  {
+    path: "/gaming",
+    name: "Gaming",
+    component: () => import("./views/Gaming.vue"),
+  },
+
+  // Home
+  {
+    path: "/",
+    name: "Home",
+    component: () => import("./views/Home.vue"),
+  },
+
+  // Layer 1
+  {
+    path: "/layer-1",
+    name: "Layer1",
+    component: () => import("./views/Layer1.vue"),
+  },
+
+  // Layer 2
+  {
+    path: "/layer-2",
+    name: "Layer2",
+    component: () => import("./views/Layer2.vue"),
+  },
+
+  // Losers
+  {
+    path: "/losers",
+    name: "Losers",
+    component: () => import("./views/Losers.vue"),
+  },
+
+  // Meme
+  {
+    path: "/meme",
+    name: "Meme",
+    component: () => import("./views/Meme.vue"),
+  },
+
+  // Metaverse
+  {
+    path: "/metaverse",
+    name: "Metaverse",
+    component: () => import("./views/Metaverse.vue"),
+  },
+
+  // News
+  {
+    path: "/news",
+    name: "News",
+    component: () => import("./views/News.vue"),
+  },
+
+  // Nft
+  {
+    path: "/nft",
+    name: "Nft",
+    component: () => import("./views/Nft.vue"),
+  },
+
+  // Not Found
+  {
+    path: "/:pathMatch(.*)",
+    name: "NotFound",
+    component: () => import("./views/NotFound.vue"),
+  },
+
+  // Privacy
+  {
+    path: "/privacy",
+    name: "Privacy",
+    component: () => import("./views/Privacy.vue"),
+  },
+
+  // Read News (Article page)
+  {
+    path: "/read:id",
+    name: "ReadNews",
+    component: () => import("./views/ReadNews.vue"),
+    props: true,
+  },
+
+  // Social
+  {
+    path: "/social",
+    name: "Social",
+    component: () => import("./views/Social.vue"),
+  },
+
+  // Stable Coin
+  {
+    path: "/stablecoin",
+    name: "Stablecoin",
+    component: () => import("./views/Stablecoin.vue"),
+  },
+
+  // Staking
+  {
+    path: "/staking",
+    name: "Staking",
+    component: () => import("./views/Staking.vue"),
+  },
+
+  // Web3
+  {
+    path: "/web3",
+    name: "Web3",
+    component: () => import("./views/Web3.vue"),
+  },
+
+  // Wrapped
+  {
+    path: "/wrapped",
+    name: "Wrapped",
+    component: () => import("./views/Wrapped.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/NotFound.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="container">
+        <div class="page-settings">
+            <div class="row error-container">
+                <div class="center-content">
+                    <!-- Animation -->
+                    <i class="fas fa-exclamation-triangle fa-5x animated-icon icon"></i>
+                    <!-- 404 Message -->
+                    <div>
+                        <h2>Ooops...</h2>
+                        <p>The page you are looking for doesn't exist or has been moved</p>
+                        <!-- CTA -->
+                        <div class="cta-container">
+                            <router-link to="/" class="cta">Take Me Home</router-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.error-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100.2vh;
+}
+
+.center-content {
+    text-align: center;
+}
+
+.icon {
+    color: #0e8900;
+}
+
+h2 {
+    font-size: 40px;
+    font-weight: 700;
+    color: #0e8900;
+}
+
+p {
+    font-weight: 700;
+    color: rgb(69, 66, 66);
+    margin-top: 16px;
+}
+
+.cta-container {
+    margin-top: 32px;
+}
+
+.cta {
+    text-decoration: none;
+    padding: 12px 24px;
+    color: white;
+    background-color: #0e8900;
+    font-weight: 700;
+    font-size: 14px;
+}
+
+.cta:hover {
+    background-color: rgba(14, 137, 0, 0.9);
+}
+
+.animated-icon {
+    animation: swing 1s infinite;
+}
+
+@keyframes swing {
+
+    0%,
+    100% {
+        transform: rotate(0deg);
+    }
+
+    20%,
+    60% {
+        transform: rotate(15deg);
+    }
+
+    40%,
+    80% {
+        transform: rotate(-15deg);
+    }
+}
+
+@import "~animate.css/animate.min.css";
+</style>
