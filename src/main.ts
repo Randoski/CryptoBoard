86456e9b27bd22d0c6a80c703b002024952760c7
randoksi <monyerandy15@gmail.com>
2024-08-13 08:38:11 +0100

    More updates

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import './style.css'
 import 'flowbite';
+import { createPinia } from 'pinia';
 
+
+const pinia = createPinia();
 const app = createApp(App)
+
+app.use(pinia);
 app.use(router)
 app.mount('#app')
 
